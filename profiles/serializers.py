5ec822a5d5f65b0123f1f4bc10a060ac8df0a5ba
Erik <erikhgm@gmail.com>
2022-11-02 10:52:37 +0000

    feat: update profile and post serializers

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,15 +1,28 @@
 from rest_framework import serializers
 from .models import Profile
+from followers.models import Follower
 
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    follower_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_follower_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(
+                owner=user, followed=obj.owner
+            ).first()
+            return following.id if following else None
+        return None
+
+        
     class Meta:
         model = Profile
         fields = '__all__'
+    
